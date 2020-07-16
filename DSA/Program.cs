5e8eb8433786eb629b18d53ace2f8ef93eb5761f
Smruti Ranjan Panda <smpanda@microsoft.com>
2020-07-16 17:34:31 +0530

    Add project files.

---
+++ DSA/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    public class Node
+    {
+        public int data;
+
+        public Node leftChild;
+        public Node rightChild;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node root = new Node(1);
+            root.leftChild = new Node(2);
+            root.rightChild = new Node(3);
+
+            root.leftChild.leftChild = new Node(4);
+            root.leftChild.rightChild = new Node(5);
+
+            Console.WriteLine("In Order Traversal");
+            TreeTraversal.InOrderTraversal(root);
+
+            Console.WriteLine("In Order Traversal Iterative");
+            TreeTraversal.InOrderTraversalIterative(root);
+
+            Console.WriteLine("Pre Order Traversal");
+            TreeTraversal.PreOrderTraversal(root);
+
+            Console.WriteLine("Post Order Traversal");
+            TreeTraversal.PostOrderTraversal(root);
+
+            Console.Read();
+        }
+    }
+}
---
+++ DSA/TreeTraversal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    public class TreeTraversal
+    {
+        public static void InOrderTraversal(Node root)
+        {
+            if (root == null)
+                return;
+            InOrderTraversal(root.leftChild);
+            Console.WriteLine(root.data);
+            InOrderTraversal(root.rightChild);
+        }
+
+        public static void PreOrderTraversal(Node root)
+        {
+            if (root == null)
+                return;
+
+            Console.WriteLine(root.data);
+            PreOrderTraversal(root.leftChild);
+            PreOrderTraversal(root.rightChild);
+        }
+
+        public static void PostOrderTraversal(Node root)
+        {
+            if (root == null)
+                return;
+            PostOrderTraversal(root.leftChild);
+            PostOrderTraversal(root.rightChild);
+            Console.WriteLine(root.data);
+        }
+
+        public static void InOrderTraversalIterative(Node root)
+        {
+            if (root == null)
+                return;
+
+            Stack<Node> stack = new Stack<Node>();
+            var node = root;
+
+            while(node != null || stack.Count() > 0)
+            {
+                if(node != null)
+                {
+                    stack.Push(node);
+                    node = node.leftChild;
+                }
+                else
+                {
+                    var ele = stack.Pop();
+                    Console.WriteLine(ele.data);
+                    node = ele.rightChild;
+                }
+            }
+        }
+    }
+}
